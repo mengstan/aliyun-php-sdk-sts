7b30cbfbf149fb80f909f9de06f8fe597bd7301b
stan <stan.liu@realmax.com>
2018-09-14 14:41:03 +0800

    init

--- src/Sts.php
+++ src/Sts.php
@@ -7,22 +7,34 @@
  */
 
 namespace mengstan\aliyunSts;
-use mengstan\aliyunCore\Profile\DefaultProfile;
 
-class Sts extends AssumeRoleRequest
+use mengstan\aliyunCore\Profile\DefaultProfile;
+use mengstan\aliyunCore\DefaultAcsClient;
+use yii\base\Component;
+
+class Sts extends Component
 {
     public $client;
 
-    public function __construct($config)
-    {
-        parent::__construct();
-        $this->setRoleArn($config['roleArn']);
-        $this->setPolicy($config['policy']);
-        $this->setRoleSessionName("client_name");
-        $this->setDurationSeconds($config['tokenExpire']);
+    public $config;
+
+    private $request;
+   
+    public function init(){
         // 初始化用户Profile实例
-        $ClientProfile = DefaultProfile::getProfile("cn-hangzhou",$config['accessKeyId'], $config['accessKeySecret']);
+        $ClientProfile = DefaultProfile::getProfile("cn-hangzhou", $this->config['AccessKeyID'], $this->config['AccessKeySecret']);
         // 初始化AcsClient用于发起请求
         $this->client = new DefaultAcsClient($ClientProfile);
+        // 初始化请求参数
+        $this->request = new AssumeRoleRequest();
+        $this->request->setRoleArn($this->config['RoleArn']);
+        $this->request->setPolicy($this->config['Policy']);
+        $this->request->setRoleSessionName("client_name");
+        $this->request->setDurationSeconds($this->config['TokenExpireTime']);
+    }
+
+    public function getStsToken()
+    {
+        return $this->client->getAcsResponse($this->request);
     }
 }
